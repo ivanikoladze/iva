db4f426ae9d5d6952598d1f2540a47458fe62890
ivanikoladze <iwanikoladze@gmail.com>
2023-01-25 04:18:43 +0400

    react app 3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react";
+
+const generateUsers = () => {
+ 
+   return [
+     {name: "iva", surname: "asatiani", age: 35, id: 1},
+     {name: "rati", surname: "burbu", age: 39, id: 2},
+     {name: "shuba", surname: "shoki", age: 95, id: 3},
+     {name: "iva", surname: "asatiani", age: 35, id: 4},
+     {name: "rati", surname: "burbu", age: 39, id: 5},
+     {name: "shuba", surname: "shoki", age: 95, id: 6},
+     {name: "iva", surname: "asatiani", age: 35, id: 7},
+     {name: "rati", surname: "burbu", age: 39, id: 8},
+     {name: "shuba", surname: "shoki", age: 95, id: 9},
+     {name: "manana", surname: "tvaradze", age: 45, id: 10},
+   ]
+ }
+
+const removeUser = (arr) =>{
+  const randomNum = Math.floor(Math.random()*arr.length);
+  const newArr = arr.filter((_,index)=> {
+    if(randomNum === index){
+      return false;
+    }else{
+      return true;
+    }
+  })
+  return newArr;
+}
 
 function App() {
+  const [users, setUsers] = useState([])
+  useEffect(()=>{
+    setUsers(generateUsers());
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users.map(item=>{return <h2 key={item.id}> name: {item.name} surname: {item.surname} age: {item.age}</h2>})}
+
+      <button onClick={(e => {
+        const randomFitlered = removeUser(users);
+        setUsers(randomFitlered);
+      })}> remove
+        </button>
+        <p> {users.length} users left</p>
     </div>
+
   );
 }
 
